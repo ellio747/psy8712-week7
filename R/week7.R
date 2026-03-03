ad4dd48239002b0acda343d702dd46b490494828
Ben Elliott <ellio747@umn.edu>
2026-03-03 08:37:34 -0600

    updated comments

--- R/week7.R
+++ R/week7.R
@@ -6,16 +6,16 @@ library(tidyverse)
 # library()
 
 # Data Import and Cleaning
-week7_tbl <- read_csv("../data/week3.csv") %>% 
+week7_tbl <- read_csv("../data/week3.csv", col_names = TRUE) %>%  #used read_csv as in csv format
   mutate(
-    timeStart = ymd_hms(timeStart),
-    condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control")),
-    gender = factor(gender, levels = c("M", "F"), labels = c("Male", "Female")),
-    timeSpent = timeEnd - timeStart,
-    across(q1:q10, as.integer)
+    timeStart = ymd_hms(timeStart), # recoded TimeStart as a POSIXct; Note: timeEnd already in POSIXct
+    condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control")), # factor coded condition according to Table 1
+    gender = factor(gender, levels = c("M", "F"), labels = c("Male", "Female")), # factor coded gender according to Table 1
+    timeSpent = timeEnd - timeStart, #diffdate = difference between two time points
+    across(q1:q10, as.integer) # Sets q1-q10 as integer
   ) %>% 
-  filter(q6 == 1) %>%
-  select(-q6)
+  filter(q6 == 1) %>% # Retains only those that answered `1` to q6 (manipulation check)
+  select(-q6) # Removes q6
 
 
 # Visualization
