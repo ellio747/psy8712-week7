a05e4e449670b7e3f82894dc097bd041ece78337
Ben Elliott <ellio747@umn.edu>
2026-03-13 07:29:16 -0500

    add save images

--- R/week7.R
+++ R/week7.R
@@ -9,6 +9,7 @@ library(GGally)
 week7_tbl <- read_csv("../data/week3.csv", col_names = TRUE) %>%  #used read_csv as in csv format
   mutate(
     timeStart = ymd_hms(timeStart), # recoded TimeStart as a POSIXct; Note: timeEnd already in POSIXct
+    #timeEnd = ymd_hms(timeEnd), # could use this just to be safe
     condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control")), # factor coded condition according to Table 1
     gender = factor(gender, levels = c("M", "F"), labels = c("Male", "Female")), # factor coded gender according to Table 1
     timeSpent = timeEnd - timeStart, #diffdate = difference between two time points
@@ -17,21 +18,20 @@ week7_tbl <- read_csv("../data/week3.csv", col_names = TRUE) %>%  #used read_csv
   filter(q6 == 1) %>% # Retains only those that answered `1` to q6 (manipulation check)
   select(-q6) # Removes q6
 
-
 # Visualization
 week7_tbl %>% 
-  select(q1:q10) %>% 
-  ggpairs(lower = list(continuous = "points"), upper = list(continuous = "cor"), diag = list(continuous = "densityDiag"))
+  ggpairs(columns = 5:13, lower = list(continuous = "points"), upper = list(continuous = "cor"), diag = list(continuous = "densityDiag"))
+ggsave(filename = "../figs/ggally.fig.png", height=3, width=4, units="in", dpi=600)
 ggplot(week7_tbl, aes(timeStart, q1)) + 
   geom_point() +
-  xlab("Date of Experiment")+
-  ylab("Q1 Score")+
+  xlab("Date of Experiment") +
+  ylab("Q1 Score") +
   labs(title = "Fig 1")
-
+ggsave(filename = "../figs/fig1.png", height=3, width=4, units="in", dpi=600)
 ggplot(week7_tbl, aes(q1, q2, color = gender)) +
   geom_point(position = "jitter") +
   labs(title = "Fig 2")
-
+ggsave(filename = "../figs/fig2.png", height=3, width=4, units="in", dpi=600)
 
 ggplot(week7_tbl, aes(q1, q2)) +
   geom_point(position = "jitter") +
@@ -39,13 +39,13 @@ ggplot(week7_tbl, aes(q1, q2)) +
   xlab("Score on Q1") +
   ylab("Score on Q2") +
   labs(title = "Fig 3")
-
+ggsave(filename = "../figs/fig3.png", height=3, width=4, units="in", dpi=300)
 ggplot(week7_tbl, aes(gender, timeSpent)) +
   geom_boxplot() +
   xlab("Gender") +
   ylab("Time elapsed (mins)") +
   labs(title = "Fig 4")
-
+ggsave(filename = "../figs/fig4.png", height=3, width=4, units="in", dpi=600)
 ggplot(week7_tbl, aes(q5, q7, color = condition)) + 
   geom_point(position = "jitter") +
   stat_smooth(method = "lm", se = FALSE) +
@@ -56,3 +56,4 @@ ggplot(week7_tbl, aes(q5, q7, color = condition)) +
   xlab("Score on Q5") +
   ylab("Score on Q7") +
   labs(title = "Fig 5")
+ggsave(filename = "../figs/fig5.png", height=3, width=4, units="in", dpi=600)
